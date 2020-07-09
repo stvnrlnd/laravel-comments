2ee98de218a4346ecffbe458435397f0ce1937a2
Steven Roland <steven@mojave.io>
2020-07-09 19:18:10 -0400

    Create comments component

---
+++ resources/js/components/Comments.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+
+          }
+        },
+    }
+</script>
+
+<style>
+
+</style>
